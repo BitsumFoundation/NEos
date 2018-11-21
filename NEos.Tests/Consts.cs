31b7b033caed9ca65659d29538f8dbe0329b284f
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-11-21 23:13:57 +0500

    +Tests for generating and importing keys

---
+++ NEos.Tests/Consts.cs
@@ -0,0 +1,17 @@
+﻿namespace NEos.Tests
+{
+    public static class Consts
+    {
+        public const string MainChainId = "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906";
+
+        public const string JungleChainId = "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca";
+
+        public const string K1PrivateKey = "5KVG5zRbKPRfMttnL9YR1r4BgC3EVwXuLE3k72HtxLCFrbr2J6s";
+
+        public const string K1PublicKey = "EOS67eM4U5Wn4Pc9isCyu46F6HVKFg8haFqwRce7gFM7FVdB8s99D";
+
+        public const string R1PrivateKey = "PVT_R1_2cvMwNJCPhkt9yvEQtPFpK8KTyuNZ8mhWdSUBCdTqhZEeXdVe8";
+
+        public const string R1PublicKey = "PUB_R1_7owzePUJWDuSnpUUFo22w2D3oJYHfauTxk8ib6HECMgjooqQws";
+    }
+}
---
+++ NEos.Tests/KeysTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NEos.Tests
+{
+    using Cryptography;
+    using System.Diagnostics;
+    using static Consts;
+
+    [TestClass]
+    public class KeysTests
+    {
+        [TestMethod]
+        public void GenerateKeysTest()
+        {
+            PrivateKey pvt = new PrivateKey(KeyTypes.K1);
+            PublicKey pub = new PublicKey(pvt);
+            Trace.WriteLine($"{pvt}\n{pub}\n");
+
+            pvt = new PrivateKey(KeyTypes.R1);
+            pub = new PublicKey(pvt);
+            Trace.WriteLine($"{pvt}\n{pub}");
+        }
+
+        [TestMethod]
+        public void K1CustomKeyTest()
+        {
+            string pk = "5KQN22arZG2NuGYveHxS3YRyNazvNxQaC381UpspWG8nxVpCs3C";
+            string expected = "EOS7gkQ28VCtq4YBEUvLiWsP2Ww16PWsAFNb2oiLk69g9QFkuTRuu";
+
+            PrivateKey pvt = new PrivateKey(pk);
+            PublicKey pub = new PublicKey(pvt);
+            Assert.AreEqual(expected, pub.ToString());
+        }
+
+        [TestMethod]
+        public void K1KeyTest()
+        {
+            PrivateKey pvt = new PrivateKey(K1PrivateKey);
+            PublicKey pub = new PublicKey(pvt);
+            Assert.AreEqual(K1PublicKey, pub.ToString());
+
+            PublicKey pub_import = new PublicKey(K1PublicKey);
+
+            Assert.AreEqual(pub.Value.ToHex(), pub_import.Value.ToHex());
+        }
+
+        [TestMethod]
+        public void R1KeyTest()
+        {
+            PrivateKey pvt = new PrivateKey(R1PrivateKey);
+            PublicKey pub = new PublicKey(pvt);
+            Assert.AreEqual(R1PublicKey, pub.ToString());
+
+            PublicKey pub_import = new PublicKey(R1PublicKey);
+
+            Assert.AreEqual(pub.Value.ToHex(), pub_import.Value.ToHex());
+        }
+    }
+}
