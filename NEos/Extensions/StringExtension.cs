70dd271c12b99723b38b4360a6acd9205741c020
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-11-21 16:06:13 +0500

    +StringExtension

---
+++ NEos/Extensions/StringExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NEos
+{
+    using Cryptography.Internal;
+
+    public static class StringExtension
+    {
+        public static byte[] FromHex(this string value)
+        {
+            byte[] result = new byte[value.Length / 2];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = (byte)Convert.ToInt32(value.Substring(i * 2, 2), 16);
+            }
+
+            return result;
+        }
+
+        public static byte[] FromBase58(this string value)
+        {
+            return Base58.Decode(value);
+        }
+    }
+}
