36002842f5ba6fc49d56f9b639c85ac406a8c138
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-11-21 16:03:59 +0500

    +Base58 converter

---
+++ NEos/Cryptography/Base58.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace NEos.Cryptography
+{
+    public static class Base58
+    {
+        private const string Hexdigits = "0123456789abcdefABCDEF";
+        private const string Digits = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+        public static string Encode(byte[] data)
+        {
+            // Decode byte[] to BigInteger
+            BigInteger intData = 0;
+            for (var i = 0; i < data.Length; i++)
+            {
+                intData = intData * 256 + data[i];
+            }
+
+            // Encode BigInteger to Base58 string
+            var result = "";
+            while (intData > 0)
+            {
+                var remainder = (int)(intData % 58);
+                intData /= 58;
+                result = Digits[remainder] + result;
+            }
+
+            // Append `1` for each leading 0 byte
+            for (var i = 0; i < data.Length && data[i] == 0; i++)
+            {
+                result = '1' + result;
+            }
+
+            return result;
+        }
+
+        public static byte[] Decode(string base58)
+        {
+            // Decode Base58 string to BigInteger 
+            BigInteger intData = 0;
+            for (var i = 0; i < base58.Length; i++)
+            {
+                var digit = Digits.IndexOf(base58[i]); //Slow
+                if (digit < 0)
+                    throw new FormatException($"Invalid Base58 character `{base58[i]}` at position {i}");
+                intData = intData * 58 + digit;
+            }
+
+            // Encode BigInteger to byte[]
+            // Leading zero bytes get encoded as leading `1` characters
+            var leadingZeroCount = base58.TakeWhile(c => c == '1').Count();
+            var leadingZeros = Enumerable.Repeat((byte)0, leadingZeroCount);
+            var bytesWithoutLeadingZeros =
+                intData.ToByteArray()
+                .Reverse()// to big endian
+                .SkipWhile(b => b == 0);//strip sign byte
+            var result = leadingZeros.Concat(bytesWithoutLeadingZeros).ToArray();
+
+            return result;
+        }
+    }
+}
