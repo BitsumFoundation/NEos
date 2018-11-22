a0f31e0ae0e4dd8735fc2cb16e2f3caa4a4d20c9
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-11-22 14:11:49 +0500

    +KeyPair class

---
+++ NEos/Cryptography/KeyPair.cs
@@ -0,0 +1,40 @@
+﻿namespace NEos.Cryptography
+{
+    public class KeyPair
+    {
+        public KeyPair(KeyTypes keyType)
+        {
+            KeyType = keyType;
+            PrivateKey = new PrivateKey();
+            PublicKey = new PublicKey(PrivateKey);
+        }
+
+        public KeyPair(string privateKey)
+        {
+            PrivateKey = new PrivateKey(privateKey);
+            PublicKey = new PublicKey(PrivateKey);
+            KeyType = PrivateKey.KeyType;
+        }
+
+        public KeyTypes KeyType { get; private set; }
+
+        public PrivateKey PrivateKey { get; private set; }
+
+        public PublicKey PublicKey { get; private set; }
+
+        public override string ToString()
+        {
+            return PublicKey.ToString();
+        }
+
+        public string SignData(string chainId, string data)
+        {
+            return PrivateKey.SignData(chainId, data);
+        }
+
+        public bool VerifySignature(string chainId, string data, string signature)
+        {
+            return PublicKey.VerifySignature(chainId, data, signature);
+        }
+    }
+}
