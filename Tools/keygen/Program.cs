36ed9cd1ce3be49cb1fe5fd1389c05d7e448eb96
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-11-23 11:34:56 +0500

    +keygen toolkit (.NET Core and .NET Framework)

---
+++ Tools/keygen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace keygen
+{
+    using Eos.Cryptography;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                KeyPair keyPair = new KeyPair(KeyTypes.K1);
+
+                Console.Clear();
+                Console.WriteLine("EOS Key pair generator:\n");
+                Console.WriteLine("K1 Key pair:");
+                Console.WriteLine($"{keyPair.PrivateKey}\n{keyPair.PublicKey}\n");
+
+                keyPair = new KeyPair(KeyTypes.R1);
+                Console.WriteLine("R1 Key pair:");
+                Console.WriteLine($"{keyPair.PrivateKey}\n{keyPair.PublicKey}\n");
+
+                Console.Write("Press any key to generate new pairs or ESC to exit: ");
+
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape) break;
+            }
+
+        }
+    }
+}
