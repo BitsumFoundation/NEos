cf65e8079909fa45b4e843178099bf707b21fee6
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-11-26 17:33:26 +0500

    +extensions

---
+++ Eos.Api/Extensions/BinaryWriterExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace Eos
+{
+    public static class BinaryWriterExtension
+    {
+        public static void WriteVarUInt32(this BinaryWriter writer, uint value)
+        {
+            while (true)
+            {
+                var val = (byte)(value & 0x7f);
+                var remaining = (byte)(value >> 7);
+
+                if (remaining > 0)
+                {
+                    writer.Write((byte)(0x80 | val));
+                }
+                else
+                {
+                    writer.Write(val);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Eos.Api/Extensions/DateTimeExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eos
+{
+    public static class DateTimeExtension
+    {
+        public static uint ToUnixTime(this DateTime dateTime)
+        {
+            return (uint)(dateTime.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+    }
+}
