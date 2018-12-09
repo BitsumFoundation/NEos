976c321b0ac05d4cc104e6a00c27505ceac38fe3
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-12-09 10:45:57 +0500

    +Buy RAM Action

---
+++ Eos.Api/Models/Actions/BuyRamAction.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace Eos.Models
+{
+    public class BuyRamData
+    {
+        [JsonProperty("payer")]
+        public Name Payer { get; set; }
+
+        [JsonProperty("receiver")]
+        public Name Receiver { get; set; }
+
+        [JsonProperty("bytes")]
+        public int Bytes { get; set; }
+    }
+
+    public class BuyRamAction : Action<BuyRamData>
+    {
+        public override Name Account => "eosio";
+
+        public override Name Name => "buyrambytes";
+    }
+}
