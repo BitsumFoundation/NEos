3e3d275d49260a01a47625a8f178d560c1a810a4
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-12-09 10:47:31 +0500

    +Unstake CPU/Net action

---
+++ Eos.Api/Models/Actions/UnstakeAction.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace Eos.Models
+{
+    public class UnstakeData
+    {
+        [JsonProperty("from")]
+        public Name From { get; set; }
+
+        [JsonProperty("receiver")]
+        public Name Receiver { get; set; }
+
+        [JsonProperty("unstake_net_quantity")]
+        public Currency Net { get; set; }
+
+        [JsonProperty("unstake_cpu_quantity")]
+        public Currency Cpu { get; set; }
+
+        [JsonProperty("transfer")]
+        public bool Transfer { get; set; }
+    }
+
+    public class UnstakeAction : Action<UnstakeData>
+    {
+        public override Name Account => "eosio";
+
+        public override Name Name => "undelegatebw";
+    }
+}
